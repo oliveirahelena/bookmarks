81b48bd2cd7dedd210cc3b7335a12b9ab44b79f7
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-08-27 05:04:20 -0300

    add reset password

--- account/urls.py
+++ account/urls.py
@@ -1,17 +1,8 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
-    # change password urls
-    path('password_change/',
-         auth_views.PasswordChangeView.as_view(),
-         name='password_change'),
-    path('password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(),
-         name='password_change_done'),
+    path('', include('django.contrib.auth.urls')),
 ]
