82eda93a69c6e7f9808cb5f800d2a2717248ca69
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-08-27 05:23:59 -0300

    add registration

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
